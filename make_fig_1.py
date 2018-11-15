e3dc061dcd8f7484420daf0ed60de176743d9b65
Anonymous <>
2018-11-15 15:50:59 +0000

    Add code to repro figure 1

---
+++ make_fig_1.py
@@ -0,0 +1,61 @@
+import numpy as np
+import torch
+import rans
+from benchmark_compressors import mnist_binarized, bz2_compress, pimg_compress
+
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox, TransformedBbox
+from matplotlib.image import BboxImage
+
+from torch_vae.torch_bin_mnist_compress import vae_append
+
+rng = np.random.RandomState(0)
+
+fig, ax = plt.subplots()
+
+N = 30
+spacing = 20
+h = 28
+
+raw_data = mnist_binarized(rng)[:N]
+
+def bytes_to_img(byts):
+    arr = np.unpackbits(np.frombuffer(byts, dtype='uint8'))
+    arr = np.concatenate((arr, np.ones(28 - arr.size % 28)))
+    return np.reshape(arr, (h, -1), order='F')
+
+bz2_compressed = bytes_to_img(bz2_compress(raw_data))
+png_compressed = bytes_to_img(pimg_compress(optimize=True)(raw_data))
+
+other_bits = rng.randint(1 << 32, size=12, dtype=np.uint32)
+state = rans.unflatten(other_bits)
+
+images = torch.from_numpy(raw_data.astype('float32'))
+images = [image.view(-1) for image in images]
+
+for image in images:
+    state = vae_append(state, image)
+bb_ans_compressed = bytes_to_img(rans.flatten(state).tobytes())
+
+raw_data = np.concatenate(~raw_data, axis=1)
+
+labels = ["BB-ANS",          "bz2",          "PNG",          "Binarized\nMNIST"]
+data   = [bb_ans_compressed, bz2_compressed, png_compressed, raw_data          ]
+
+yticks = []
+for i, d in enumerate(data):
+    bbox = Bbox.from_bounds(0, 28 * (spacing + i * (h + spacing)),
+                            28 * d.shape[1], 28 * h)
+    bbox = TransformedBbox(bbox, ax.transData)
+    bbox_image = BboxImage(bbox, cmap='gray', origin=None)
+    bbox_image.set_data(d)
+    ax.add_artist(bbox_image)
+    yticks.append(28 * (spacing + i * (h + spacing) + h // 2))
+
+ax.set_xlim(0, max(28 * d.shape[1] for d in data))
+ax.set_yticks(yticks)
+ax.set_yticklabels(labels)
+ax.set_ylim(0, 28 * (spacing + (i + 1) * (h + spacing)))
+ax.set_xlabel('Size (bits)')
+ax.set_aspect('equal')
+plt.savefig('compression_plot.png', dpi=200, bbox_inches='tight')
--- torch_vae/torch_bin_mnist_compress.py
+++ torch_vae/torch_bin_mnist_compress.py
@@ -25,6 +25,13 @@ compress_lengths = []
 latent_shape = (40,)
 model = BinaryVAE(hidden_dim=100, latent_dim=40)
 
+if randomise_data:
+    model.load_state_dict(
+        torch.load('torch_vae/saved_params/torch_binary_vae_params_random'))
+else:
+    model.load_state_dict(
+        torch.load('torch_vae/saved_params/torch_binary_vae_params'))
+
 rec_net = tvae_utils.torch_fun_to_numpy_fun(model.encode)
 gen_net = tvae_utils.torch_fun_to_numpy_fun(model.decode)
 
@@ -36,62 +43,59 @@ vae_append = util.vae_append(latent_shape, gen_net, rec_net, obs_append,
 vae_pop = util.vae_pop(latent_shape, gen_net, rec_net, obs_pop,
                        prior_precision, q_precision)
 
-# load some mnist images
-mnist = datasets.MNIST('data/mnist', train=False, download=True,
-                       transform=transforms.Compose(
-                           [transforms.ToTensor()]))
-images = mnist.test_data[:num_images]
+if __name__ == '__main__':
+    # load some mnist images
+    mnist = datasets.MNIST('data/mnist', train=False, download=True,
+                           transform=transforms.Compose(
+                               [transforms.ToTensor()]))
+    images = mnist.test_data[:num_images]
 
-if randomise_data:
-    images = Bernoulli(images.float() / 255.).sample()
-    model.load_state_dict(
-        torch.load('torch_vae/saved_params/torch_binary_vae_params_random'))
-else:
-    images = torch.round(images.float() / 255.)
-    model.load_state_dict(
-        torch.load('torch_vae/saved_params/torch_binary_vae_params'))
+    if randomise_data:
+        images = Bernoulli(images.float() / 255.).sample()
+    else:
+        images = torch.round(images.float() / 255.)
 
-images = [image.view(-1) for image in images]
+    images = [image.view(-1) for image in images]
 
-# randomly generate some 'other' bits
-other_bits = rng.randint(low=1 << 16, high=1 << 31, size=20, dtype=np.uint32)
-state = rans.unflatten(other_bits)
+    # randomly generate some 'other' bits
+    other_bits = rng.randint(low=1 << 16, high=1 << 31, size=20, dtype=np.uint32)
+    state = rans.unflatten(other_bits)
 
-print_interval = 10
-encode_start_time = time.time()
-for i, image in enumerate(images):
-    state = vae_append(state, image)
+    print_interval = 10
+    encode_start_time = time.time()
+    for i, image in enumerate(images):
+        state = vae_append(state, image)
 
-    if not i % print_interval:
-        print('Encoded {}'.format(i))
+        if not i % print_interval:
+            print('Encoded {}'.format(i))
 
-    compressed_length = 32*(len(rans.flatten(state)) - len(other_bits)) / (i+1)
-    compress_lengths.append(compressed_length)
+        compressed_length = 32*(len(rans.flatten(state)) - len(other_bits)) / (i+1)
+        compress_lengths.append(compressed_length)
 
-print('\nAll encoded in {:.2f}s'.format(time.time() - encode_start_time))
-compressed_message = rans.flatten(state)
+    print('\nAll encoded in {:.2f}s'.format(time.time() - encode_start_time))
+    compressed_message = rans.flatten(state)
 
-compressed_bits = 32 * (len(compressed_message) - len(other_bits))
-print("Used " + str(compressed_bits) +
-      " bits.")
-print('This is {:.2f} bits per pixel'.format(compressed_bits / (num_images*784.)))
+    compressed_bits = 32 * (len(compressed_message) - len(other_bits))
+    print("Used " + str(compressed_bits) +
+          " bits.")
+    print('This is {:.2f} bits per pixel'.format(compressed_bits / (num_images*784.)))
 
-if not os.path.exists('results'):
-    os.mkdir('results')
-np.savetxt('results/compressed_lengths_bin', np.array(compress_lengths))
+    if not os.path.exists('results'):
+        os.mkdir('results')
+    np.savetxt('results/compressed_lengths_bin', np.array(compress_lengths))
 
-state = rans.unflatten(compressed_message)
-decode_start_time = time.time()
+    state = rans.unflatten(compressed_message)
+    decode_start_time = time.time()
 
-for n in range(len(images)):
-    state, image_ = vae_pop(state)
-    original_image = images[len(images)-n-1].numpy()
-    assert all(original_image == np.array(image_))
+    for n in range(len(images)):
+        state, image_ = vae_pop(state)
+        original_image = images[len(images)-n-1].numpy()
+        assert all(original_image == np.array(image_))
 
-    if not n % print_interval:
-        print('Decoded {}'.format(n))
+        if not n % print_interval:
+            print('Decoded {}'.format(n))
 
-print('\nAll decoded in {:.2f}s'.format(time.time() - decode_start_time))
+    print('\nAll decoded in {:.2f}s'.format(time.time() - decode_start_time))
 
-recovered_bits = rans.flatten(state)
-assert all(other_bits == recovered_bits)
+    recovered_bits = rans.flatten(state)
+    assert all(other_bits == recovered_bits)
