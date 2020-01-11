85898aad6f7627aa22b0d894e58d27b0b27d022a
Jamie Townsend <jamiehntownsend@gmail.com>
2020-01-11 19:01:46 +0000

    Improve docstrings in rans.py

--- rans.py
+++ rans.py
@@ -1,18 +1,44 @@
 """
-Closely based on https://github.com/rygorous/ryg_rans/blob/master/rans64.h
+Closely based on https://github.com/rygorous/ryg_rans/blob/master/rans64.h by
+Fabian Giesen.
 
-ROUGH GUIDE:
-We use the pythonic names 'append' and 'pop' for encoding and decoding
-respectively. The compressed state 'x' is an immutable stack, implemented using
-a cons list.
+We use the pythonic names `append` and `pop` for encoding and decoding
+respectively. The compressed state is a pair `msg = (head, tail)`, where `head`
+is an int in the range `[0, 2 ** head_precision)` and `tail` is an immutable
+stack, implemented using a cons list, containing ints in the range
+`[0, 2 ** tail_precision)`. The precisions must satisfy
 
-x: the current stack-like state of the encoder/decoder.
+  head_precision > tail_precision.
 
-precision: the natural numbers are divided into ranges of size 2^precision.
+For convenient compatibility with Numpy dtypes we use the settings
+head_precision = 64 and tail_precision = 32.
 
-start & freq: start indicates the beginning of the range in [0, 2^precision-1]
-that the current symbol is represented by. freq is the length of the range.
-freq is chosen such that p(symbol) ~= freq/2^precision.
+Both the `append` method and the `pop` method assume access to a probability
+distribution over symbols. We use the name `symb` for a symbol. To describe the
+probability distribution we model the real interval [0, 1] with the range of
+integers {0, 1, 2, ..., 2 ** precision}. Each symbol is represented by a
+sub-interval within that range. For a set of symbols {a, b, c, d} this looks
+like this:
+
+    0                                                             1
+    |          |----- P(symb) ------|                             |
+    |                                                             |
+    |    a           symb == b           c              d         |
+    |----------|--------------------|---------|-------------------|
+    |                                                             |
+    |          |------ prob --------|                             |
+    0        start                                            2 ** precision
+
+As shown in the above diagram, the number `prob` represents the width of the
+interval, corresponding to `symb`, so that
+
+  P(symb) = prob / 2 ** precision
+
+where P is the probability mass function of our distribution.
+
+The number `start` represents the beginning of the interval corresponding to
+`symb`, which is analagous to the cumulative distribution function evaluated on
+`symb`.
 """
 import numpy as np
 from functools import reduce
@@ -24,36 +50,36 @@ tail_mask = (1 << tail_precision) - 1
 head_min  = 1 << head_precision - tail_precision
 
 #        head    , tail
-x_init = head_min, ()
+msg_init = head_min, ()
 
-def append(x, start, freq, precision):
+def append(msg, start, prob, precision):
     """
-    Encodes a symbol with range [`start`, `start + freq`).  All frequencies are
-    assumed to sum to `1 << precision`, and compressed bits get written to x.
+    Encodes a symbol with range `[start, start + prob)`.  All `prob`s are
+    assumed to sum to `2 ** precision`. Compressed bits get written to `msg`.
     """
     # Prevent Numpy scalars leaking in
-    start, freq, precision = map(int, [start, freq, precision])
-    head, tail = x
-    if head >= freq << head_precision - precision:
+    start, prob, precision = map(int, [start, prob, precision])
+    head, tail = msg
+    if head >= prob << head_precision - precision:
         # Need to push data down into tail
         head, tail = head >> tail_precision, (head & tail_mask, tail)
-    return (head // freq << precision) + head % freq + start, tail
+    return (head // prob << precision) + head % prob + start, tail
 
-def pop(x, statfun, precision):
+def pop(msg, statfun, precision):
     """
-    Pops a symbol from x. The signiature of statfun should be
-        statfun: cf |-> symbol, (start, freq)
-    where `cf` is in the interval [`start`, `freq`) and `symbol` is the symbol
+    Pops a symbol from msg. The signiature of statfun should be
+        statfun: cf |-> symb, (start, prob)
+    where `cf` is in the interval `[start, prob)` and `symb` is the symbol
     corresponding to that interval.
     """
     # Prevent Numpy scalars leaking in
     precision = int(precision)
-    head, tail = x
+    head, tail = msg
     cf = head & ((1 << precision) - 1)
-    symb, (start, freq) = statfun(cf)
+    symb, (start, prob) = statfun(cf)
     # Prevent Numpy scalars leaking in
-    start, freq = int(start), int(freq)
-    head = freq * (head >> precision) + cf - start
+    start, prob = int(start), int(prob)
+    head = prob * (head >> precision) + cf - start
     if head < head_min:
         # Need to pull data up from tail
         head_new, tail = tail
@@ -61,25 +87,25 @@ def pop(x, statfun, precision):
     return (head, tail), symb
 
 def append_symbol(statfun, precision):
-    def append_(x, symbol):
-        start, freq = statfun(symbol)
-        return append(x, start, freq, precision)
+    def append_(msg, symbol):
+        start, prob = statfun(symbol)
+        return append(msg, start, prob, precision)
     return append_
 
 def pop_symbol(statfun, precision):
-    def pop_(x):
-        return pop(x, statfun, precision)
+    def pop_(msg):
+        return pop(msg, statfun, precision)
     return pop_
 
-def flatten(x):
-    """Flatten a rans state x into a 1d numpy array."""
-    out, x = [x[0] >> 32, x[0]], x[1]
-    while x:
-        x_head, x = x
+def flatten(msg):
+    """Flatten a rANS message into a 1d numpy array."""
+    out, msg = [msg[0] >> 32, msg[0]], msg[1]
+    while msg:
+        x_head, msg = msg
         out.append(x_head)
     return np.asarray(out, dtype=np.uint32)
 
 def unflatten(arr):
-    """Unflatten a 1d numpy array into a rans state."""
+    """Unflatten a 1d numpy array into a rANS message."""
     return (int(arr[0]) << 32 | int(arr[1]),
             reduce(lambda tl, hd: (int(hd), tl), reversed(arr[2:]), ()))
